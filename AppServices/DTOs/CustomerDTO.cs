bcbb7db99b4df8bd0ca45666e1ae94e7c817a475
Gustavo Deola <gustavodeola@gmail.com>
2022-05-30 18:29:18 -0300

    feat

---
+++ AppServices/DTOs/CustomerDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServices.DTOs
+{
+    public class CustomerDTO
+    {
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string EmailConfirmation { get; set; }
+        public string Cpf { get; set; }
+        public int idade { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string CellPhone { get; set; }
+        public bool EmailSms { get; set; }
+        public bool Whatsapp { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ AppServices/Helpers/CustomerProfile.cs
@@ -0,0 +1,19 @@
+﻿using APIDesafioWarren.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppServices.DTOs;
+    
+namespace AppServices.Helpers
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+        }
+    }
+}
