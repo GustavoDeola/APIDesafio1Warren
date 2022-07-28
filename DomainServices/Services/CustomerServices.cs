a9c6efc35a713a570cda946b4a379afe23ef000b
GustavoDeola <gustavo.deola@warren.com.br>
2022-07-28 17:33:20 -0300

    refactor: GetBy e GetAll

--- DomainServices/Services/CustomerServices.cs
+++ DomainServices/Services/CustomerServices.cs
@@ -6,79 +6,96 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Domain.Services
 {
     public class CustomerServices : ICustomerServices
     {
-        private readonly Context _context;
-
         private readonly IUnitOfWork _unitOfWork;
 
         private readonly IRepositoryFactory _repositoryFactory;
-        public CustomerServices(IUnitOfWork unitOfWork, IRepositoryFactory repositoryFactory)
+        public CustomerServices(IUnitOfWork<Context> unitOfWork, IRepositoryFactory<Context> repositoryFactory)
         {
             _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
             _repositoryFactory = repositoryFactory ?? throw new ArgumentNullException(nameof(repositoryFactory));
         }
 
-        public IEnumerable<Customer> GetAll(Func<Customer, bool> predicate = null)
+        public IEnumerable<Customer> GetAll(Expression<Func<Customer, bool>> predicate = null)
         {
             var repository = _repositoryFactory.Repository<Customer>();
 
             var query = repository.MultipleResultQuery();
+                    
 
             var result = repository.Search(query);
 
             return result;
+        }
 
-            /*if (predicate is null) return _context.Customers;
+        public Customer GetBy(params Expression<Func<Customer, bool>>[] predicate)
+        {
+            var repository = _repositoryFactory.Repository<Customer>();
 
-            var customers = _context.Customers.Where(predicate);
-            return customers;
+            var query = repository.SingleResultQuery();
+                                  //.AndFilter(predicate);
+
+            //var result = repository.FirstOrDefault(query);
+
+
+           // return result;
+            
+            foreach (var item in predicate)
+            {
+                query.AndFilter(item);
+            }
+
+            return repository.FirstOrDefault(query);
+            
+            /*var customer = _context.Customers.AsNoTracking().FirstOrDefault(predicate);
+            return customer;
             */
         }
 
-        public Customer GetBy(Func<Customer, bool> predicate)
-        {
-            var customer = _context.Customers.AsNoTracking().FirstOrDefault(predicate);
-            return customer;
-        }
-
         public int Add(Customer customer)
-        {
+        { 
             if (AnyCustomerForCpf(customer)) return -1;
 
-            _context.Customers.Add(customer);
+            var repository = _unitOfWork.Repository<Customer>();
 
-            _context.SaveChanges();
+            repository.Add(customer);
+
+            _unitOfWork.SaveChanges();
             return customer.Id;
         }
 
         public bool Update(Customer customerToUpdate)
         {
             var findCustomers = GetBy(c => c.Id == customerToUpdate.Id);
+            var repository = _unitOfWork.Repository<Customer>();
             if (findCustomers is null && AnyCustomerForCpf(customerToUpdate)) return false;
 
-            _context.Customers.Update(customerToUpdate);
-            _context.SaveChanges();
+            repository.Update(customerToUpdate);
+            _unitOfWork.SaveChanges();
             return true;
         }
 
         public bool Remove(int id)
         {
             var customer = GetBy(c => c.Id == id);
+            var repository = _unitOfWork.Repository<Customer>();
             if (customer == null) return false;
 
-            _context.Customers.Remove(customer);
-            _context.SaveChanges();
+            repository.Remove(customer);
+            _unitOfWork.SaveChanges();
 
             return true;
         }
 
         private bool AnyCustomerForCpf(Customer customer)
         {
-            return _context.Customers.Any(c => c.Cpf == customer.Cpf);
+            var repository = _unitOfWork.Repository<Customer>();
+            return repository.Any(c => c.Cpf == customer.Cpf);
         } 
     }
 }
--- DomainServices/Services/ICustomerServices.cs
+++ DomainServices/Services/ICustomerServices.cs
@@ -3,15 +3,16 @@ using Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Domain.Services
 {
     public interface ICustomerServices
     { 
-        IEnumerable<Customer> GetAll(Func<Customer, bool> predicate = null);
+        IEnumerable<Customer> GetAll(Expression<Func<Customer, bool>> predicate = null);
         public int Add(Customer customer);
         public bool Update(Customer customerToUpdate);
         public bool Remove(int id);
-        Customer GetBy(Func<Customer, bool> predicate);
+        Customer GetBy(params Expression<Func<Customer, bool>>[] predicate);
     }
 }
