6a58dd16b0d2ee0b571ac2d87f8f072b187f79c7
GustavoDeola <gustavo.deola@warren.com.br>
2022-08-03 18:11:51 -0300

    feat: criação das entidades e props

---
+++ Domain Models/Portfolio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models
+{
+    
+    public class Portfolio
+    {
+        public Portfolio(
+            decimal totalBalance, 
+            ICollection<Product> products
+            )
+        {
+            TotalBalance = totalBalance;
+            Products = products;
+        }
+
+        public decimal TotalBalance { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Domain Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models
+{
+    public class Product
+    {
+        public Product(
+            string symbol, 
+            int quotes, 
+            decimal unitPrice, 
+            decimal netValue, 
+            DateTime convertedAt
+            )
+        {
+            Symbol = symbol;
+            Quotes = quotes;
+            UnitPrice = unitPrice;
+            NetValue = netValue;
+            ConvertedAt = convertedAt;
+        }
+
+        public string Symbol{ get; set; }
+
+        public int Quotes { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal NetValue { get; set; }
+
+        public DateTime ConvertedAt { get; set; }
+    }
+}
